8cfeda2d63724c5e712c1734a71cda8201fb3f04
isra <onawa21@hotmail.com>
2024-10-10 09:42:18 +0100

    Ahora si terminado

--- informacion.php
+++ informacion.php
@@ -5,6 +5,6 @@
     echo "Hola ".$_SESSION['nombre']."<br>";
     echo "Tienes la edad de ".$_SESSION['edad']."<br>";
     echo "Con el email " .$_SESSION['email']."<br>";
-    echo "Su pais es: ".$_SESSION['pais'] = $pais;
+    echo "Su pais es: ".$_SESSION['pais'];
 
 ?>
