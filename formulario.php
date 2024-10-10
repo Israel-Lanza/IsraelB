800dece1dced17bdbf9131de2ac6a5a53bb74389
isra <onawa21@hotmail.com>
2024-10-10 08:35:58 +0100

    Creación del formulario

--- formulario.php
+++ formulario.php
@@ -16,7 +16,21 @@
 <body>
 
 
-    <form action="formulario.php" method="post"></form>
+    <form action="formulario.php" method="post">
+        <label for="usuario">Usuario: </label>
+        <input type="text" name="usuario"><br>
+
+        <label for="email">Email: </label>
+        <input type="email" name="email"><br>
+
+        <label for="edad">Edad: </label>
+        <input type="text" name="edad"><br>
+
+        <label for="pais">Pais: </label>
+        <input type="text" name="pais"><br>
+        
+        <input type="submit" value="Enviar">
+    </form>
     
 </body>
 </html>
